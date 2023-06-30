c3b0b2a0032caa1230d6046b91e863fb3007d055
Shreshtha <shreshthgupta@gmail.com>
2023-07-01 02:58:26 +0530

    Added side section with filters and sorting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import ProductList from './features/product-list/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ProductList></ProductList>
     </div>
   );
 }
