87f6c7ccda1bfa78cefa33ebc002a982e9b9b604
Shreshtha <shreshthgupta@gmail.com>
2023-07-01 15:53:36 +0530

    Added Login and Signup pages

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,11 @@
+import Signup from "../features/auth/authComponents/Signup";
+
+function SignupPage() {
+  return (
+    <div>
+      <Signup></Signup>
+    </div>
+  );
+}
+
+export default SignupPage;
