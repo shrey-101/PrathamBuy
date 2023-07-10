283b6db270ce284a9daf26f88f4eef9a9e0fb9d9
Shreshtha <shreshthgupta@gmail.com>
2023-07-10 15:07:38 +0530

    Order Sucess and my orders Pages added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectLoggedInUser } from "./features/auth/authSlice";
 import { fetchItemsByUserIdAsync } from "./features/cart/cartSlice";
+import PageNotFound from "./pages/404";
+import OrderSuccessPage from "./pages/OrderSuccessPage";
+import UserOrders from "./features/user/components/UserOrders";
+import UserOrdersPage from "./pages/UserOrdersPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -60,23 +64,40 @@ const router = createBrowserRouter([
       </Protected>
     ),
   },
+  {
+    path: "/order-success/:id",
+    element: <OrderSuccessPage></OrderSuccessPage>,
+  },
+  {
+    path: "/orders",
+    element: (
+      <UserOrdersPage></UserOrdersPage>
+      // we will add Page later right now using component directly.
+    ),
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
 ]);
 
 function App() {
-  const dispatch = useDispatch();
-  const user = useSelector(selectLoggedInUser);
+  const dispatch = useDispatch(); //The useDispatch hook is used to access the Redux dispatch function. It allows you to dispatch actions to the Redux store.
+  const user = useSelector(selectLoggedInUser); //The useSelector hook is used to select and retrieve data from the Redux store. In this case, it is selecting the loggedInUser from the store using the selectLoggedInUser selector.
 
   useEffect(() => {
+    //The useEffect hook is utilized to handle side effects in the component. It runs a callback function when certain dependencies change.  In this code, it runs when the user object changes.
     if (user) {
-      dispatch(fetchItemsByUserIdAsync(user.id));
+      //This checks if the user object exists or is truthy.
+      dispatch(fetchItemsByUserIdAsync(user.id)); //If the user object exists, the code dispatches an action called fetchItemsByUserIdAsync with the user.id as an argument. This action is likely defined elsewhere and is responsible for fetching items associated with the logged-in user.
     }
-  }, [dispatch, user]);
+  }, [dispatch, user]); // The dependencies array [dispatch, user] is provided to the useEffect hook, which means the effect will only re-run if either the dispatch function or the user object changes.
 
   return (
     <div className="App">
       <RouterProvider router={router} />
     </div>
   );
-}
+} // Overall, this code suggests that the component is responsible for rendering the main structure of the application and initiating an API call to fetch items based on the logged-in user when the user object is available.
 
 export default App;
