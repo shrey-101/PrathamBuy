24f5792d62fd963fb64ebf284dc3ef36efcbe387
Shreshtha <shreshthgupta@gmail.com>
2023-07-12 02:24:00 +0530

    User, Cart, Orders added at Backend

--- Frontend/src/features/product/components/ProductDetail.js
+++ Frontend/src/features/product/components/ProductDetail.js
@@ -51,14 +51,12 @@ export default function ProductDetail() {
   const alert = useAlert();
   const handleCart = (e) => {
     e.preventDefault();
-    if (items.findIndex((item) => item.productId === product.id) < 0) {
+    if (items.findIndex((item) => item.product.id === product.id) < 0) {
       const newItem = {
-        ...product,
-        productId: product.id,
+        product: product.id,
         quantity: 1,
         user: user.id,
       };
-      delete newItem["id"];
       dispatch(addToCartAsync(newItem));
       alert.info("Item added to cart");
     } else {
